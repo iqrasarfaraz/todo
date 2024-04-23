8eb462e8deade08ed47dd512a8b39f4c51effb4c
iqrasarfaraz <160213971+iqrasarfaraz@users.noreply.github.com>
2024-04-23 05:51:32 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+let todos = [];
+let condition = true;
+while(condition){
+let addTask = await inquirer.prompt(
+    [
+
+        {
+            name:"todo",       
+        type:"input",
+            message:"what do you want to add in your todos?",
+
+        },
+        {
+            name:"addmore",
+            type:"confirm",
+            message:"Do you want to add more ?",
+            default:"false"
+        },
+    ]
+);
+//console.log(addTask.todo)
+todos.push(addTask.todo);
+condition = addTask.addmore;
+console.log(todos)
+
+}
